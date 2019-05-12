d5dee0dc18bd15e835c9474f6352611176d50089
Alethea Cary <aletheacary@gmail.com>
2019-05-12 02:41:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -20,9 +20,9 @@ end
   
 def while_iterator(number_of_times)
   number_of_times = 7
-  1 while number_of_times > -1 
+  while number_of_times > -1 
     puts phrase = "Welcome to Flatiron School's Web Development Course!"
-    number_of_times -= 
+    number_of_times -= 1
   end
   
 end 
